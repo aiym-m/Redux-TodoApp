1bd928117f36640a982a83460fe2539bace96832
Aiymkan <metinova.a@gmail.com>
2024-10-12 19:43:31 -0500

    Initial commit

---
+++ src/Components/Todocard/TodoCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./TodoCard.css";
+
+function TodoCard({title, userId, completed, id, onToggle, onDelete}) {
+
+
+  return (
+    <div className='todo-card'>
+      <div className='title-check'>
+        <input type="checkbox" checked={completed } 
+      onChange={() => onToggle(id)} className=''/>
+        <h2>{title}</h2>
+      </div>
+      <div className='buttons'>
+			<h3>User ID: {userId}</h3>
+			<button onClick={() => onDelete(id)}>Delete</button>
+      </div>
+    </div>
+  )
+}
+
+export default TodoCard;
---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import LoginPage from './Pages/LoginPage/LoginPage.jsx'
+import TodoList from './Components/Todolist/Todolist.jsx'
+ const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "todoList",
+        element: <TodoList/>,
+      },
+      {
+        path: "todolist/completed",
+        element: <TodoList/>,
+      },
+      {
+        path: "todolist/pending",
+        element: <TodoList/>
+      }
+
+    ],
+    
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+ ])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  
+  </StrictMode>,
+)
+
